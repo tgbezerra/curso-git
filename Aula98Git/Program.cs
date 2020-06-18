bfae82614400fd0e9d9bd472b67ba495d5430f42
Thiago Gomes <tgbezerra@gmail.com>
2020-06-18 19:26:02 -0300

    Acrescentada uma mensagem de bom dia

--- Aula98Git/Program.cs
+++ Aula98Git/Program.cs
@@ -7,6 +7,7 @@ namespace Aula98Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
